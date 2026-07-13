19dd23b96cd2ffc3bcaeb461f7f0e8cca611d9a7
MarcoAntonioPdLA <mponcedeleona@unsa.edu.pe>
2026-07-13 11:24:05 -0500

    Agregando endpoints para lectura de productos por parte de los usuarios.

--- ReFood-Backend/config/urls.py
+++ ReFood-Backend/config/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('users.urls')),
+    path('api/products/', include('products.urls')),
 ]
 
 if settings.DEBUG:
--- ReFood-Backend/products/views.py
+++ ReFood-Backend/products/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Category, Product
+from .serializers import CategorySerializer, ProductSerializer
 
 # Create your views here.
+class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class ProductViewSet(viewsets.ReadOnlyModelViewSet):
+    serializer_class = ProductSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        queryset = Product.objects.filter(is_active=True)
+        featured = self.request.query_params.get('is_featured_offer')
+        category = self.request.query_params.get('category')
+
+        if featured is not None:
+            queryset = queryset.filter(is_featured_offer=featured.lower() == 'true')
+        if category:
+            queryset = queryset.filter(category_id=category)
+
+        return queryset
